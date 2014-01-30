8bd0eb7fd0adacffd087f5f5a55c94d5211e2b23
Yuichi Ishikawa <yuichiis.pub@gmail.com>
2014-01-30 17:28:55 +0900

    add phpunit.xml

---
+++ tests/Bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+date_default_timezone_set('UTC');
+
+include 'init_autoloader.php';
+$loader->add('AcmeTest', __DIR__ . '/resources');
+$loader->add('SfTest',   __DIR__ . '/AnerisBundle/src');
+define('ANERIS_TEST_RESOURCES',__DIR__.'/resources');
+define('ANERIS_TEST_DATA',     __DIR__.'/data');
+define('ANERIS_TEST_CONFIG',   __DIR__.'/config');
+
+global $_SERVER;
+$_SERVER["KERNEL_DIR"] = __DIR__.'/AnerisBundle/app/';
+
+Aneris\Stdlib\Cache\CacheFactory::$fileCachePath = __DIR__.'/cache';
+Aneris\Stdlib\Cache\CacheFactory::$enableApcCache = false;
+Aneris\Stdlib\Cache\CacheFactory::$enableFileCache = true;
+Aneris\Stdlib\Cache\CacheFactory::clearCache();
+
